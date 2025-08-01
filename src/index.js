fbbd065c1a8c229915ad4604ba434097dab5d2ff
mukhtarkabir <mukhtarkabir3394@gmail.com>
2025-08-01 04:33:43 +0100

    Add transition and make code DRY.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const carouselFrame = document.querySelector("main");
 const nextButton = document.querySelector("button.next");
 const previousButton = document.querySelector("button.previous");
 const numberOfPictures = images.length;
-let currentPictureNumber = 1;
+let currentPictureNumber = 0;
 
 images.forEach((image) => {
   const picture = document.createElement("img");
@@ -14,33 +14,23 @@ images.forEach((image) => {
   carouselFrame.append(picture);
 });
 
-const goToNextPicture = function (number) {
+const slide = (number) => {
   const images = carouselFrame.querySelectorAll("img");
   images.forEach((image) => {
     image.style.transform = `translateX(-${number * 100}%)`;
-  });
-};
-const goToPreviousPicture = function (number) {
-  const images = carouselFrame.querySelectorAll("img");
-  images.forEach((image) => {
-    if (number == 1) {
-      image.style.transform = `translateX(${0}%)`;
-    } else {
-      image.style.transform = `translateX(-${number * 100}%)`;
-    }
   });
 };
 
 nextButton.addEventListener("click", () => {
   if (currentPictureNumber < numberOfPictures - 1) {
     currentPictureNumber += 1;
-    goToNextPicture(currentPictureNumber);
+    slide(currentPictureNumber);
   }
 });
 
 previousButton.addEventListener("click", () => {
-  if (currentPictureNumber > 1) {
+  if (currentPictureNumber > 0) {
     currentPictureNumber -= 1;
-    goToPreviousPicture(currentPictureNumber);
+    slide(currentPictureNumber);
   }
 });
